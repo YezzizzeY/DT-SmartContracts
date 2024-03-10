2bc335d604329dd5dad63ae3941c34857a8101c9
YezzizzeY <1721062927@qq.com>
2024-03-10 03:32:05 -0700

    good

--- test.sol
+++ test.sol
@@ -5,6 +5,6 @@ pragma solidity ^0.8.0;
 contract IntegerDivision {
     
     function divideByTenPowerNineteen(int256 input) public pure returns (int256) {
-        return input / 1e20;
+        return input / 1e20 +1;
     }
 }
